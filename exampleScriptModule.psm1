283def25973e93355a9f7ba3b0f868c907edf6e3
hashf0rd <124741545+hashf0rd@users.noreply.github.com>
2024-06-11 12:36:43 +0100

    removed .psm1 file

--- exampleScriptModule.psm1
+++
@@ -1,4 +0,0 @@
-    function Show-Users {
-        Get-CimInstance -ClassName Win32_Account | Where-Object { $_.SIDType -eq 1 } | Select-Object Caption, SID
-    }
-    Export-ModuleMember -Function Show-Users
